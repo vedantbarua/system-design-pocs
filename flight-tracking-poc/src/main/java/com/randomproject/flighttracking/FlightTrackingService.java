30d6e27de5c81b05ce256b38d3a62227323c00a4
Vedant Barua <vedantbarua@gmail.com>
2026-02-23 17:39:59 -0600

    fix changes

--- flight-tracking-poc/src/main/java/com/randomproject/flighttracking/FlightTrackingService.java
+++ flight-tracking-poc/src/main/java/com/randomproject/flighttracking/FlightTrackingService.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Comparator;
