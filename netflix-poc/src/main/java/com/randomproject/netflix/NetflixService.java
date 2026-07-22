91229a78eaa70f02c4906357003f2c691a78ab22
Vedant Barua <vedantbarua@gmail.com>
2026-07-22 13:02:28 -0500

    chore: clean up Java compiler warnings across POCs

--- netflix-poc/src/main/java/com/randomproject/netflix/NetflixService.java
+++ netflix-poc/src/main/java/com/randomproject/netflix/NetflixService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.time.Instant;
 import java.time.Year;
 import java.util.ArrayList;
 import java.util.Comparator;
